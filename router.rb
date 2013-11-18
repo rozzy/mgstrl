1a9c3c77a923d798011a9679210f0d59ec466e50
Rozzy <berozzy@gmail.com>
2013-11-18 14:14:45 +0200

    Router logic

--- router.rb
+++ router.rb
@@ -1,10 +1,18 @@
-get '/' do
-  slim :index
-end
-
 get '/*.css' do |file|
   if File.exists? "#{settings.views}/#{settings.styles}/#{file}.sass"
     content_type 'text/css', :charset => 'utf-8'
     sass :"#{settings.styles}/#{file}"
   else redirect '/' end
+end
+
+
+get '/*/?' do |page|
+  page = page == '' ? 'index' : page
+  if File.exists? "#{settings.views}/#{page}.slim"
+  slim page.to_sym 
+  else raise not_found end
+end
+
+not_found do
+  slim :not_found
 end
