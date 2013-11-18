6995fa9325267ffa53c9c6373cf1e88839ddd86d
Rozzy <berozzy@gmail.com>
2013-11-18 14:56:11 +0200

    Opts and args improvement. Opts [] => nil

--- functions.rb
+++ functions.rb
@@ -1,6 +1,5 @@
-def link href, content, classes = '', opts = []
+def link href, content, classes = '', opts = nil, args = ''
   active = request.env['REQUEST_PATH'] == href ? true : false
-  args = ""
-  opts.each {|key, value| args += " #{key.to_s}=\"#{value}\""} if opts != []
+  opts.each {|key, value| args += " #{key.to_s}=\"#{value}\""} if opts != nil
   "<a href=\"#{href}\" #{"onclick=\"return false;\" " if active}class=\"#{classes}#{' active' if active}\"#{args}>#{content}</a>"
 end
